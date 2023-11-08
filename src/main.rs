ccc01b5875a91d918dd6f94e859a75d1eb61471c
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-11-08 21:39:52 +0100

    Refactor parse_config

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
+
+    println!("Searching for {query}");
+    println!("{file_path}");
 
     let contents = fs::read_to_string(file_path).expect("Fail to read file");
-    println!("Searching for {query}");
-    println!("In {file_path}");
 
     println!("Contents are {contents}");
+}
+
+fn parse_config(args: &[String]) -> (String, String) {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+    (query, file_path)
 }
