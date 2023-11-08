46c5673c28e2cb24f96ff1dff0b172f47e3afde7
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-11-08 21:43:42 +0100

    Add Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,25 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {query}");
-    println!("{file_path}");
+    println!("Searching for {}", config.query);
+    println!(" In {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Fail to read file");
+    let contents = fs::read_to_string(config.file_path).expect("Fail to read file");
 
     println!("Contents are {contents}");
 }
 
-fn parse_config(args: &[String]) -> (String, String) {
+fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
-    (query, file_path)
+    Config { query, file_path }
 }
