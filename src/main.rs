7e1c9c0852ee4e1b498b53e6ebe877920137c3c2
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-11-08 21:50:25 +0100

    Add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,16 +8,22 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error when building config: {}", err);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!(" In {}", config.file_path);
