f246404578fbc0dbe6703c28ca503f9d1cbc476c
Gabriel2409 <gabriel.cournelle@gmail.com>
2023-11-08 21:32:40 +0100

    Add file reading

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let query = &args[1];
     let file_path = &args[2];
 
+    let contents = fs::read_to_string(file_path).expect("Fail to read file");
     println!("Searching for {query}");
     println!("In {file_path}");
+
+    println!("Contents are {contents}");
 }
